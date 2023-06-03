9849e0dd5adfb343585701cf21214a3baea18a8f
Dominant-Official <135420479+Dominant-Official@users.noreply.github.com>
2023-06-03 15:36:24 +0530

    Update Make a phone call.java

--- Make a phone call.java
+++ Make a phone call.java
@@ -1,1 +1,23 @@
+//below code is to make a phone call.
 
+//getting list of contacts
+Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+
+  while (cursor.moveToNext()) {
+
+	   String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+	   String phone = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+	   {
+
+		  HashMap<String, Object> _item = new HashMap<>();
+
+	      _item.put("name", name);
+
+          _item.put("phone", phone);
+
+		  contacts.add(_item);
+
+	   }
+
+  }
