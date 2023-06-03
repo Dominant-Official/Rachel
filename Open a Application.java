ace6e71d89ea09b950b1a28747c6887a4e8358a6
Dominant-Official <135420479+Dominant-Official@users.noreply.github.com>
2023-06-03 15:08:13 +0530

    Update Open a Application.java

--- Open a Application.java
+++ Open a Application.java
@@ -1,39 +1,41 @@
 //below code is use to open a app from list of installed applications.
-public ArrayList<HashMap<String, Object>> getApplicationList (final String _app_name, final String _package_name) {
+public ArrayList<HashMap<String, Object>> appList() {{
 
-				{						android.content.pm.PackageManager pm = myContext.getPackageManager();
+        PackageManager pm = myContext.getPackageManager();
 
-						     HashMap<String, Object> Map = new HashMap<>();
+	    HashMap<String, Object> Map = new HashMap<>();
+		ArrayList<HashMap<String, Object>> map = new ArrayList<>();
 
-							 ArrayList<HashMap<String, Object>> map = new ArrayList<>();
+        
 
-						    Intent intent = new Intent(Intent.ACTION_MAIN, null);
+		Intent intent = new Intent(Intent.ACTION_MAIN, null);
 
-						    intent.addCategory(Intent.CATEGORY_LAUNCHER);
+		intent.addCategory(Intent.CATEGORY_LAUNCHER);
 
-						    List<android.content.pm.ResolveInfo> PackList = pm.queryIntentActivities(intent, android.content.pm.PackageManager.PERMISSION_GRANTED);
+	    List<ResolveInfo> PackList = pm.queryIntentActivities(intent,android.content.pm.PackageManager.PERMISSION_GRANTED);
 
-						    for (android.content.pm.ResolveInfo rInfo : PackList) {
+           
 
-								
+           for (ResolveInfo rInfo : PackList) {
 
-								        String AppName = rInfo.activityInfo.applicationInfo.loadLabel(pm).toString();
+	               String AppName = rInfo.activityInfo.applicationInfo.loadLabel(pm).toString();
 
-								        String PackageName = rInfo.activityInfo.applicationInfo.packageName;
+   			    String PackageName = rInfo.activityInfo.applicationInfo.packageName;
 
-								Map = new HashMap<>();
+   				Map = new HashMap<>();
 
-								Map.put(_app_name, AppName);
+   				Map.put(app_name, AppName);
 
-								Map.put(_package_name, PackageName);
+	   			Map.put(package_name, PackageName);
 
-								map.add(Map);
+   				map.add(Map);
 
-								        }
+		    }
 
-						        return map;
+	   	        return map;     
 
-						        }
+        
 
-		}
+	}
 
+}
