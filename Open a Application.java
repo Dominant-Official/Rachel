923b50b6b05b610ea54c4f59df608a7939b12ba6
Dominant-Official <135420479+Dominant-Official@users.noreply.github.com>
2023-06-03 14:46:52 +0530

    Update Open a Application.java

--- Open a Application.java
+++ Open a Application.java
@@ -1,3 +1,39 @@
 //below code is use to open a app from list of installed applications.
+public ArrayList<HashMap<String, Object>> getApplicationList (final String _app_name, final String _package_name) {
 
+				{						android.content.pm.PackageManager pm = myContext.getPackageManager();
+
+						     HashMap<String, Object> Map = new HashMap<>();
+
+							 ArrayList<HashMap<String, Object>> map = new ArrayList<>();
+
+						    Intent intent = new Intent(Intent.ACTION_MAIN, null);
+
+						    intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+						    List<android.content.pm.ResolveInfo> PackList = pm.queryIntentActivities(intent, android.content.pm.PackageManager.PERMISSION_GRANTED);
+
+						    for (android.content.pm.ResolveInfo rInfo : PackList) {
+
+								
+
+								        String AppName = rInfo.activityInfo.applicationInfo.loadLabel(pm).toString();
+
+								        String PackageName = rInfo.activityInfo.applicationInfo.packageName;
+
+								Map = new HashMap<>();
+
+								Map.put(_app_name, AppName);
+
+								Map.put(_package_name, PackageName);
+
+								map.add(Map);
+
+								        }
+
+						        return map;
+
+						        }
+
+		}
 
